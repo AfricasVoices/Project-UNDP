f0115a2a59162c11c79c13600327be1e15248254
Alexander Simpson <as2388@cam.ac.uk>
2019-07-02 17:13:05 +0100

    Rename ahadi_s01_*.csv -> *.csv

--- run_scripts/3_generate_outputs.sh
+++ run_scripts/3_generate_outputs.sh
@@ -38,5 +38,5 @@ cd ..
     "$DATA_ROOT/Raw Data" "$DATA_ROOT/Coded Coda Files/" \
     "$DATA_ROOT/Outputs/messages_traced_data.json" "$DATA_ROOT/Outputs/individuals_traced_data.json" \
     "$DATA_ROOT/Outputs/ICR/" "$DATA_ROOT/Outputs/Coda Files/" \
-    "$DATA_ROOT/Outputs/ahadi_s01_messages.csv" "$DATA_ROOT/Outputs/ahadi_s01_individuals.csv" \
-    "$DATA_ROOT/Outputs/ahadi_s01_production.csv"
+    "$DATA_ROOT/Outputs/messages.csv" "$DATA_ROOT/Outputs/individuals.csv" \
+    "$DATA_ROOT/Outputs/production.csv"
