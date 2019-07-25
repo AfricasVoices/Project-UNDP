4d89d7df343678a948b9a2d9ece894aa565cf3e9
Alexander Simpson <as2388@cam.ac.uk>
2019-07-25 17:42:15 +0100

    Add generate_analysis_graphs.py from ADSS

---
+++ docker-run-generate-analysis-graphs.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+set -e
+
+IMAGE_NAME=undp-rco
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --profile-cpu)
+            PROFILE_CPU=true
+            CPU_PROFILE_OUTPUT_PATH="$2"
+            shift 2;;
+        --)
+            shift
+            break;;
+        *)
+            break;;
+    esac
+done
+
+
+# Check that the correct number of arguments were provided.
+if [[ $# -ne 4 ]]; then
+    echo "Usage: ./docker-run.sh
+    [--profile-cpu <profile-output-path>]
+    <user> <messages-traced-data> <individuals-traced-data> <output-dir>"
+    exit
+fi
+
+# Assign the program arguments to bash variables.
+USER=$1
+INPUT_MESSAGES_TRACED_DATA=$2
+INPUT_INDIVIDUALS_TRACED_DATA=$3
+OUTPUT_DIR=$4
+
+# Build an image for this pipeline stage.
+docker build --build-arg INSTALL_CPU_PROFILER="$PROFILE_CPU" -t "$IMAGE_NAME" .
+
+# Create a container from the image that was just built.
+if [[ "$PROFILE_CPU" = true ]]; then
+    PROFILE_CPU_CMD="pyflame -o /data/cpu.prof -t"
+    SYS_PTRACE_CAPABILITY="--cap-add SYS_PTRACE"
+fi
+CMD="pipenv run $PROFILE_CPU_CMD python -u generate_analysis_graphs.py \
+    \"$USER\" \
+    /data/messages-traced-data.json /data/individuals-traced-data.json /data/output-graphs \
+"
+container="$(docker container create ${SYS_PTRACE_CAPABILITY} -w /app "$IMAGE_NAME" /bin/bash -c "$CMD")"
+
+# Copy input data into the container
+docker cp "$INPUT_MESSAGES_TRACED_DATA" "$container:/data/messages-traced-data.json"
+docker cp "$INPUT_INDIVIDUALS_TRACED_DATA" "$container:/data/individuals-traced-data.json"
+
+# Run the container
+docker start -a -i "$container"
+
+# Copy the output data back out of the container
+mkdir -p "$OUTPUT_DIR"
+docker cp "$container:/data/output-graphs/." "$OUTPUT_DIR"
+
+if [[ "$PROFILE_CPU" = true ]]; then
+    mkdir -p "$(dirname "$CPU_PROFILE_OUTPUT_PATH")"
+    docker cp "$container:/data/cpu.prof" "$CPU_PROFILE_OUTPUT_PATH"
+fi
+
+# Tear down the container, now that all expected output files have been copied out successfully
+docker container rm "$container" >/dev/null
---
+++ generate_analysis_graphs.py
@@ -0,0 +1,117 @@
+import argparse
+from collections import OrderedDict
+
+import altair
+from core_data_modules.logging import Logger
+from core_data_modules.traced_data.io import TracedDataJsonIO
+from core_data_modules.util import IOUtils
+
+from src.lib import PipelineConfiguration
+
+Logger.set_project_name("UNDP_RCO")
+log = Logger(__name__)
+
+IMG_SCALE_FACTOR = 10  # Increase this to increase the resolution of the outputted PNGs
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generates graphs for analysis")
+
+    parser.add_argument("user", help="User launching this program")
+    parser.add_argument("messages_json_input_path", metavar="messages-json-input-path",
+                        help="Path to a JSONL file to read the TracedData of the messages data from")
+    parser.add_argument("individuals_json_input_path", metavar="individuals-json-input-path",
+                        help="Path to a JSONL file to read the TracedData of the messages data from")
+    parser.add_argument("output_dir", metavar="output-dir",
+                        help="Directory to write the output graphs to")
+
+    args = parser.parse_args()
+
+    user = args.user
+    messages_json_input_path = args.messages_json_input_path
+    individuals_json_input_path = args.individuals_json_input_path
+    output_dir = args.output_dir
+
+    IOUtils.ensure_dirs_exist(output_dir)
+
+    PipelineConfiguration.RQA_CODING_PLANS = PipelineConfiguration.BOSSASO_RQA_CODING_PLANS
+
+    # Read the messages dataset
+    log.info(f"Loading the messages dataset from {messages_json_input_path}...")
+    with open(messages_json_input_path) as f:
+        messages = TracedDataJsonIO.import_jsonl_to_traced_data_iterable(f)
+    log.info(f"Loaded {len(messages)} messages")
+
+    # Read the individuals dataset
+    log.info(f"Loading the individuals dataset from {individuals_json_input_path}...")
+    with open(individuals_json_input_path) as f:
+        individuals = TracedDataJsonIO.import_jsonl_to_traced_data_iterable(f)
+    log.info(f"Loaded {len(individuals)} individuals")
+
+    # Compute the number of messages in each show and graph
+    log.info(f"Graphing the number of messages received in response to each show...")
+    messages_per_show = OrderedDict()  # Of radio show index to messages count
+    for plan in PipelineConfiguration.RQA_CODING_PLANS:
+        messages_per_show[plan.raw_field] = 0
+
+    for msg in messages:
+        for plan in PipelineConfiguration.RQA_CODING_PLANS:
+            if msg.get(plan.raw_field, "") != "" and msg["consent_withdrawn"] == "false":
+                messages_per_show[plan.raw_field] += 1
+
+    chart = altair.Chart(
+        altair.Data(values=[{"show": k, "count": v} for k, v in messages_per_show.items()])
+    ).mark_bar().encode(
+        x=altair.X("show:N", title="Show", sort=list(messages_per_show.keys())),
+        y=altair.Y("count:Q", title="Number of Messages")
+    ).properties(
+        title="Messages per Show"
+    )
+    chart.save(f"{output_dir}/messages_per_show.html")
+    chart.save(f"{output_dir}/messages_per_show.png", scale_factor=IMG_SCALE_FACTOR)
+
+    # Compute the number of individuals in each show and graph
+    log.info(f"Graphing the number of individuals who responded to each show...")
+    individuals_per_show = OrderedDict()  # Of radio show index to individuals count
+    for plan in PipelineConfiguration.RQA_CODING_PLANS:
+        individuals_per_show[plan.raw_field] = 0
+
+    for ind in individuals:
+        for plan in PipelineConfiguration.RQA_CODING_PLANS:
+            if ind.get(plan.raw_field, "") != "" and ind["consent_withdrawn"] == "false":
+                individuals_per_show[plan.raw_field] += 1
+
+    chart = altair.Chart(
+        altair.Data(values=[{"show": k, "count": v} for k, v in individuals_per_show.items()])
+    ).mark_bar().encode(
+        x=altair.X("show:N", title="Show", sort=list(individuals_per_show.keys())),
+        y=altair.Y("count:Q", title="Number of Individuals")
+    ).properties(
+        title="Individuals per Show"
+    )
+    chart.save(f"{output_dir}/individuals_per_show.html")
+    chart.save(f"{output_dir}/individuals_per_show.png", scale_factor=IMG_SCALE_FACTOR)
+
+    # Plot the per-season distribution of responses for each survey question, per individual
+    for plan in PipelineConfiguration.SURVEY_CODING_PLANS:
+        for cc in plan.coding_configurations:
+            if cc.analysis_file_key is None:
+                continue
+
+            log.info(f"Graphing the distribution of codes for {cc.analysis_file_key}...")
+            label_counts = OrderedDict()
+            for code in cc.code_scheme.codes:
+                label_counts[code.string_value] = 0
+
+            for ind in individuals:
+                label_counts[ind[cc.analysis_file_key]] += 1
+
+            chart = altair.Chart(
+                altair.Data(values=[{"label": k, "count": v} for k, v in label_counts.items()])
+            ).mark_bar().encode(
+                x=altair.X("label:N", title="Label", sort=list(label_counts.keys())),
+                y=altair.Y("count:Q", title="Number of Individuals")
+            ).properties(
+                title=f"Season Distribution: {cc.analysis_file_key}"
+            )
+            chart.save(f"{output_dir}/season_distribution_{cc.analysis_file_key}.html")
+            chart.save(f"{output_dir}/season_distribution_{cc.analysis_file_key}.png", scale_factor=IMG_SCALE_FACTOR)
