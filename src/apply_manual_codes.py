5f4e79ecbd047779d865be61ccdf1e3d3d925ccd
Alexander Simpson <as2388@cam.ac.uk>
2019-08-07 15:04:44 +0100

    Fix import error

--- src/apply_manual_codes.py
+++ src/apply_manual_codes.py
@@ -7,6 +7,7 @@ from core_data_modules.traced_data import Metadata
 from core_data_modules.traced_data.io import TracedDataCodaV2IO
 
 from src.lib import PipelineConfiguration
+from src.lib.code_schemes import CodeSchemes
 from src.lib.pipeline_configuration import CodingModes
 
 
